c9c9a77b4e242e9539b1a8074b164dd9c70693d1
simonvau <simon@norapps.no>
2025-09-01 12:37:54 +0200

    Initial Commit

---
+++ ios-26-navbar-bug-example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-26-navbar-bug-example
+//
+//  Created by Simon Vaular on 01/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
